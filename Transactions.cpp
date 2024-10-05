1b96ced1d6f6cf69c082e20c39fbf811d45fc398
Luv Valecha <139698035+Luv-valecha@users.noreply.github.com>
2024-10-05 17:57:45 +0530

    Added transactions class

--- Transactions.cpp
+++ Transactions.cpp
@@ -2,4 +2,23 @@
 using namespace std;
 
 // This class represents individual transactions, with attributes such as date, amount, category, and description.
-class Transaction{};
+class Transaction{
+public:
+    string date; // DD-MM-YYYY
+    double amount;
+    string category;
+    string description;
+
+    //add category after converting the string in lowercase
+    Transaction(const string &date, double amount, const string &cat)
+        : date(date), amount(amount), category(cat), description("") {};
+
+    Transaction(const string &date, double amount, const string &cat, const string &desc)
+        : date(date), amount(amount), category(cat), description(desc) {};
+
+    // Helper to display a transaction
+    void printTransaction() const{
+        cout << "Date: " << date << ", Amount: " << amount << "INR"
+                  << ", Category: " << category << ", Description: " << description << endl;
+    }
+};
