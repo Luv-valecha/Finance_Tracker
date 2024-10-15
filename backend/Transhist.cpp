815b7923890100b644eac148e228efbb15e7e57c
Luv Valecha <139698035+Luv-valecha@users.noreply.github.com>
2024-10-16 00:45:32 +0530

    adding transaction storage and processing logic

---
+++ backend/Transhist.cpp
@@ -0,0 +1,252 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include "Transactions.h"
+#include "Transhist.h"
+#ifdef _WIN32
+    #include <direct.h> // For creating directories on Windows
+#else
+    #include <sys/stat.h> // For creating directories on UNIX-based systems
+    #include <sys/types.h>
+#endif
+
+using namespace std;
+
+// This class maintains a doubly linked list of transactions for efficient transaction management.
+/*
+Features:
+-Add transaction
+-Remove last transaction ***
+-Summary of last n transactions
+-Summary filtered by category
+-Summary for range of date
+-Statistical summary
+-Graphical Report
+*/
+
+Transaction_history::Transaction_history()
+{
+    // dummy head and tail
+    head = new Transaction_node(new Transaction("", -1, "", ""));
+    tail = new Transaction_node(new Transaction("", -1, "", ""));
+    head->next = tail;
+    tail->prev = head;
+    number_of_transactions = 0;
+}
+
+void Transaction_history::new_transaction(Transaction *transaction, string username)
+{
+    Transaction_node *temp = new Transaction_node(transaction); // Creating a new node
+    // Ensure correct linking
+    temp->next = tail;
+    temp->prev = tail->prev;
+    (tail->prev)->next = temp;
+    (tail->prev) = temp;
+    number_of_transactions++;
+    std::string directory = "user_transaction_details";
+    // Create the directory if it doesn't exist
+    #ifdef _WIN32
+        _mkdir(directory.c_str()); // For Windows
+    #else
+        mkdir(directory.c_str(), 0777); // For UNIX-based systems
+    #endif
+
+    std::string filename = directory + "/" + username + ".txt";
+    std::cout << "opening file: " << filename;
+    std::ofstream file(filename, std::ios_base::app); 
+    file << transaction->date << " " << transaction->amount << " " << transaction->category << " " << transaction->description << "\n";
+    file.close();
+}
+
+void Transaction_history::makedll(Transaction *transaction)
+{
+    Transaction_node *temp = new Transaction_node(transaction); // Creating a new node
+    temp->next = tail;
+    temp->prev = tail->prev;
+    (tail->prev)->next = temp;
+    (tail->prev) = temp;
+}
+
+bool Transaction_history::empty()
+{
+    return head->next == tail;
+}
+
+bool Transaction_history::bringtransactions(string username) {
+    string directory = "user_transaction_details";
+    string filename = directory + "/" + username + ".txt";
+    std::ifstream file(filename);
+    if (!file.is_open())
+        return false;
+
+    string date, category;
+    double amount;
+    std::string description;
+
+    while (file >> date >> amount >> category) {
+        // Read the remaining line as the description
+        std::getline(file >> std::ws, description); // Read the rest of the line including spaces
+        Transaction *temp = new Transaction(date, amount, category, description);
+        makedll(temp);
+    }
+    file.close();
+    return true;
+}
+
+
+vector<string> Transaction_history::PrintAllTransactions(string username)
+{
+    vector<string> transactions;
+    if (!bringtransactions(username))
+    {
+        transactions.push_back("No Transactions Available");
+        return transactions;
+    }
+    if (empty())
+    {
+        transactions.push_back("No Transactions Available");
+        return transactions;
+    }
+    Transaction_node *curr = head->next;
+    while (curr != tail)
+    {
+        stringstream ss;
+        ss << "Date: " << curr->transaction->date << ", Amount: " << curr->transaction->amount
+           << " INR, Category: " << curr->transaction->category
+           << ", Description: " << curr->transaction->description;
+        transactions.push_back(ss.str());
+        curr = curr->next;
+    }
+    return transactions;
+}
+
+vector<string> Transaction_history::PrintNTransactions(int n)
+{
+    vector<string> transactions;
+    if (empty() || number_of_transactions < n)
+    {
+        transactions.push_back("Invalid Number of Transactions");
+        return transactions;
+    }
+
+    int start_printing_from = number_of_transactions - n;
+    Transaction_node *curr = head->next;
+
+    while (start_printing_from)
+    {
+        curr = curr->next;
+        start_printing_from--;
+    }
+
+    while (curr != tail)
+    {
+        stringstream ss;
+        ss << "Date: " << curr->transaction->date << ", Amount: " << curr->transaction->amount
+           << " INR, Category: " << curr->transaction->category
+           << ", Description: " << curr->transaction->description;
+        transactions.push_back(ss.str());
+        curr = curr->next;
+    }
+    return transactions;
+}
+
+vector<string> Transaction_history::PrintCategoryWise(string target,string username)
+{
+    vector<string> transactions;
+    if (!bringtransactions(username))
+    {
+        transactions.push_back("No Transactions Available");
+        return transactions;
+    }
+    if (empty())
+    {
+        transactions.push_back("No Transactions Available");
+        return transactions;
+    }
+
+    int nothing_printed = 1;
+    Transaction_node *curr = head->next;
+    while (curr != tail)
+    {
+        if (curr->transaction->category == target)
+        {
+            stringstream ss;
+            ss << "Date: " << curr->transaction->date << ", Amount: " << curr->transaction->amount
+               << " INR, Category: " << curr->transaction->category
+               << ", Description: " << curr->transaction->description;
+            transactions.push_back(ss.str());
+            nothing_printed = 0;
+        }
+        curr = curr->next;
+    }
+
+    if (nothing_printed)
+        transactions.push_back("No Transactions Available");
+    return transactions;
+}
+
+vector<int> Transaction_history::extract(string currdate)
+{
+    int date = 0, month = 0, year = 0;
+    date = (currdate[8] - '0') * 10 + (currdate[9] - '0');
+    month = (currdate[5] - '0') * 10 + (currdate[6] - '0');
+    year = (currdate[0] - '0') * 1000 + (currdate[1] - '0') * 100 + (currdate[2] - '0') * 10 + (currdate[3] - '0');
+    return {date, month, year};
+}
+
+// YYYY-MM-DD format
+vector<string> Transaction_history::PrintDateRange(string from, string to,string username)
+{
+    vector<string> transactions;
+    if (!bringtransactions(username))
+    {
+        transactions.push_back("No Transactions Available");
+        return transactions;
+    }
+    if (empty())
+    {
+        transactions.push_back("No Transactions Available");
+        return transactions;
+    }
+
+    int nothing_printed = 1;
+    Transaction_node *curr = head->next;
+    while (curr != tail)
+    {
+        string currdate = curr->transaction->date;
+        vector<int> credentials = extract(currdate);
+        vector<int> from_credentials = extract(from);
+        vector<int> to_credentials = extract(to);
+        if (credentials[2] < from_credentials[2])
+        {
+            curr = curr->next;
+            continue;
+        }
+        if (credentials[1] < from_credentials[1])
+        {
+            curr = curr->next;
+            continue;
+        }
+        if (credentials[0] < from_credentials[0])
+        {
+            curr = curr->next;
+            continue;
+        }
+        if (credentials[2] > to_credentials[2])
+            break;
+        if (credentials[1] > to_credentials[1])
+            break;
+        if (credentials[0] > to_credentials[0])
+            break;
+        stringstream ss;
+        ss << "Date: " << curr->transaction->date << ", Amount: " << curr->transaction->amount
+           << " INR, Category: " << curr->transaction->category
+           << ", Description: " << curr->transaction->description;
+        transactions.push_back(ss.str());
+        nothing_printed = 0;
+        curr = curr->next;
+    }
+
+    if (nothing_printed)
+        transactions.push_back("No Transactions Available");
+    return transactions;
+}
---
+++ backend/Transhist.h
@@ -0,0 +1,41 @@
+#ifndef TRANSHIST_H
+#define TRANSHIST_H
+
+#include <bits/stdc++.h>
+#include <fstream>
+#include "Transactions.h" 
+using namespace std;
+
+// Linked list node definition
+class Transaction_node {
+public:
+    Transaction *transaction;
+    Transaction_node *next;
+    Transaction_node *prev;
+
+    Transaction_node(Transaction *transaction)
+        : transaction(transaction), prev(nullptr), next(nullptr) {}
+};
+
+// This class maintains a doubly linked list of transactions for efficient transaction management.
+class Transaction_history {
+private:
+    Transaction_node *head;
+    Transaction_node *tail;
+    int number_of_transactions;
+
+public:
+    Transaction_history();
+
+    void new_transaction(Transaction *transaction,string username);
+    bool empty();
+    vector<string> PrintAllTransactions(string username);
+    vector<string> PrintNTransactions(int n);
+    vector<string> PrintCategoryWise(string target,string username);
+    vector<string> PrintDateRange(string from, string to, string username);
+    vector<int> extract(string currdate);
+    bool bringtransactions(string username);
+    void makedll(Transaction* transaction);
+};
+
+#endif // TRANSHIST_H
